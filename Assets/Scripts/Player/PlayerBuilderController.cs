0bb3cc4c923d3cb7e2285903bb34c03015328d3f
Martín Salvador Sosa <104036719+Tinincho@users.noreply.github.com>
2025-01-26 00:31:16 -0300

    La concha de la lora

---
+++ Assets/Scripts/Player/PlayerBuilderController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBuilderController : MonoBehaviour
+{
+    private int positionX = 0;
+    private int positionY = 0;
+    private Vector3 targetPosition;
+
+    private void OnMovement(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        targetPosition = transform.position + new Vector3(movementVector.x, 0, movementVector.y);
+        targetPosition = new Vector3(Mathf.Round(movementVector.x), 0, Mathf.Round(movementVector.y));
+        transform.position += targetPosition;
+    }
+
+    private void OnRotate()
+    {
+        transform.Rotate(0, 90f, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Colliding!");
+    }
+}
